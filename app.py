d9df07bdeb7764c7c9d8c46bf64f7907b6723a6e
Pranav Soni <142740716+pranavsoni21@users.noreply.github.com>
2026-03-03 14:16:29 +0530

    Add function to sum two numbers and print result

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+def add_numbers(a, b):
+  sum_result = a + b
+  return sum_result
+
+
+num1 = 5
+num2 = 10
+
+result = add_numbers(num1, num2)
+
+print(f"The sum of {num1} and {num2} is {result}")
 
